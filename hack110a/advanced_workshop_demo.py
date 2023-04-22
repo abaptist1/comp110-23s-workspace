7b3ad794424438cca97ff27990aa04642839903c
Alex Baptiste <abaptist@unc.edu>
2023-04-21 21:38:57 -0400

    hack110 workshops 2

---
+++ hack110a/advanced_workshop_demo.py
@@ -0,0 +1,21 @@
+import pygame
+import sys
+from constants import *
+from projectile import Projectile
+
+# Starts window
+pygame.init()
+
+# creates clock bject to limit FPS
+FramePerSec = pygame.time.Clock()
+
+# create surface
+
+displaysurfae = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display
+
+
+proj: Projectile
+power_start
+
+
