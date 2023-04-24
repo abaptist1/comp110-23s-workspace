ef512bb6507664b5e6b12a267f4eacd0d6928281
Alex Baptiste <abaptist@unc.edu>
2023-04-24 15:56:47 -0400

    Finished EX10

--- exercises/ex10/Simpy.py
+++ exercises/ex10/Simpy.py
@@ -8,29 +8,27 @@ __author__ = "730473328"
 
 
 class Simpy:
+    """Defines the Simpy Class."""
+
     values: list[float]
-
-
     # TODO: Your constructor and methods will go here.
-    def __init__(self, values):
-        """Initializes the values attribute of Simpy to the argument passed in."""
+
+    def __init__(self, values: list[float]):
+        """Initialize the values attribute of Simpy to the argument passed in."""
         self.values = values
 
-    
     def __str__(self) -> str:
-        """Converts Simpy Object into String Representation."""
+        """Convert Simpy Object into String Representation."""
         return f"Simpy({str(self.values)})"
     
-    
     def fill(self, val_filled: float, num_vals_to_fill: int) -> None:
-        """Fills a Simpy's values with a repeating value."""
+        """Fill Simpy's values with a repeating value."""
         self.values = [val_filled] * num_vals_to_fill
 
-    def arange(self, start: float, stop: float, step = 1):
-        """Arranges a range of values in order by a certain step value."""
+    def arange(self, start: float, stop: float, step: float = 1) -> None:
+        """Arrange a range of values in order by a certain step value."""
         assert step != 0.0
         self.values = []
-        value = start
         if step > 0:
             while start < stop:
                 self.values.append(start)
@@ -40,14 +38,12 @@ class Simpy:
                 self.values.append(start)
                 start += step
         
-
     def sum(self) -> float:
-        """Sums all the values in a Simpy Object."""
+        """Sum all the values in a Simpy Object."""
         return sum(self.values)
     
-
     def __add__(self, rhs: Union[float, Simpy]) -> Simpy:
-        """Sums individual values in a Simpy object."""
+        """Sum individual values in a Simpy object."""
         new_simpy = Simpy(self.values.copy())
         if type(rhs) == float:
             new_simpy.values = [val + rhs for val in self.values]
@@ -57,9 +53,8 @@ class Simpy:
                 new_simpy.values[i] += rhs.values[i]
         return new_simpy
 
-
     def __pow__(self, rhs: Union[float, Simpy]) -> Simpy:
-        """Raises one veach value in self to the power of the corresponding Simpy Value."""
+        """Raise one veach value in self to the power of the corresponding Simpy Value."""
         new_simpy = Simpy(self.values.copy())
         if type(rhs) == float:
             new_simpy.values = [val ** rhs for val in self.values]
@@ -69,9 +64,8 @@ class Simpy:
                 new_simpy.values[i] = self.values[i] ** rhs.values[i]
         return new_simpy
 
-
     def __eq__(self, rhs: Union[float, Simpy]) -> list[bool]:
-        """Checks to see if values are equal at corresponding indexes of a Simpy."""
+        """Check to see if values are equal at corresponding indexes of a Simpy."""
         mask: list[bool] = []
         if isinstance(rhs, float):
             for val in range(len(self.values)):
@@ -88,7 +82,7 @@ class Simpy:
         return mask
     
     def __gt__(self, rhs: Union[float, Simpy]) -> list[bool]:
-        """Checks to see if values in self are greater than corresponding values in rhs."""
+        """Check to see if values in self are greater than corresponding values in rhs."""
         mask: list[bool] = []
         if isinstance(rhs, float):
             for i in range(len(self.values)):
@@ -105,7 +99,7 @@ class Simpy:
         return mask
     
     def __getitem__(self, rhs: Union[int, list[bool]]) -> Union[float, Simpy]:
-        """Retrieves a specific item from a Simpy."""
+        """Retrieve a specific item from a Simpy."""
         if isinstance(rhs, int):
             output = self.values[rhs]
             return output
@@ -115,7 +109,3 @@ class Simpy:
                 if rhs[i] is True:
                     floats.append(self.values[i])
             return Simpy(floats)
-
-
-    
-
